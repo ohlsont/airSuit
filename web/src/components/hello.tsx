db1f7f0e56e40b14f8ceabd65b063dc9a30971d5
Tomas Ohlson <ohlson.tomas@gmail.com>
2017-05-28 23:46:54 +0200

    fixed linting and added material ui

--- web/src/components/hello.tsx
+++ web/src/components/hello.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react'
+import { AppBar } from 'material-ui'
 
-export interface HelloProps { compiler: string; framework: string }
+export interface HelloProps {
+  compiler: string
+  framework: string
+}
 
 export class Hello extends React.Component<HelloProps, undefined> {
   render() {
-    return <h1>
-        Hello kkps from {this.props.compiler} and {this.props.framework}!
-    </h1>
+    return <div>
+        <AppBar
+            title="Title ddd"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <h1>
+            Hello kkps ee from {this.props.compiler} and {this.props.framework}!
+        </h1>
+    </div>
   }
 }
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import * as injectTapEventPlugin from 'react-tap-event-plugin'
 
 import { Hello } from './components/Hello'
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <MuiThemeProvider>
+        <Hello compiler="TypeScript" framework="React" />
+    </MuiThemeProvider>,
     document.getElementById('app'),
 )
